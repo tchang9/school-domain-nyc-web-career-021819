b1ce6e8aecc2cb410ed4f4863eb3680066bd6ca8
tchang9 <tlchang@gmail.com>
2019-02-20 19:19:40 -0500

    wrote Class School

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+
+    attr_accessor :name, :roster
+
+    def initialize(name, roster={})
+        @name = name
+        @roster = roster
+    end
+
+    def add_student(student_name, grade)
+        if roster.key?(grade)
+            roster[grade].push(student_name)
+        else
+            roster[grade] = [student_name]
+        end
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        roster.each do |grade, list_of_students|
+            roster[grade] = list_of_students.sort
+        end
+        return roster
+    end
+end
